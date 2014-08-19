77e6b54f9ee4ed8143f5bfd3fb58f0d41a50d820
JeffLeung <mr.jeffleung@gmail.com>
2014-08-18 20:13:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bukku::Application.config.secret_key_base = '0564c788f90356c2cbf158ee4fff44a84d3c9be4bc851e7f8a001feb362461bf4cb77d5d2eab7c593c26b13906b1a1859e8b4d15cf346a9bc20d7b49c11a56b3'
